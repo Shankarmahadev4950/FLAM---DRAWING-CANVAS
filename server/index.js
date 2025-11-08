48a6d4dcaf1779178b1a8e71cc5dd686fea20688
SHANKAR MAHADEV A <131544142+Shankarmahadev4950@users.noreply.github.com>
2025-11-08 14:57:33 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,17 +11,17 @@ const io = new Server(server, {
     cors: { origin: "*" }
 });
 
-app.use(express.static(path.join(__dirname, "..")));
 
+app.use(express.static(path.join(__dirname, "../client")));
+
+const socketClientPath = require.resolve("socket.io/client-dist/socket.io.js");
 app.get("/socket.io/socket.io.js", (req, res) => {
-    res.sendFile(path.join(__dirname, "../node_modules/socket.io/client-dist/socket.io.js"));
+    res.sendFile(socketClientPath);
 });
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "index.html"));
+    res.sendFile(path.join(__dirname, "../client/index.html"));
 });
-
 new SocketManager(io);
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 server.listen(PORT, () => console.log(`🚀 Server running on ${PORT}`));
