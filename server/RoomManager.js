346e4dca68c7282ff5b22bc45136d846b0507c9a
SHANKAR MAHADEV A <131544142+Shankarmahadev4950@users.noreply.github.com>
2025-11-09 16:51:30 +0530

    Update RoomManager.js

--- server/RoomManager.js
+++ server/RoomManager.js
@@ -1,61 +1,34 @@
 class RoomManager {
     constructor() {
-        this.rooms = new Map();
-        this.rooms.set('default', {
-            id: 'default',
-            users: new Map(),
-            history: null,
-            createdAt: Date.now()
-        });
+        this.users = new Map();
+        console.log('🏠 Room Manager initialized');
     }
 
-    getRoom(roomId = 'default') {
-        if (!this.rooms.has(roomId)) {
-            this.rooms.set(roomId, {
-                id: roomId,
-                users: new Map(),
-                history: null,
-                createdAt: Date.now()
-            });
-        }
-        return this.rooms.get(roomId);
+    addUser(userId, socketId) {
+        this.users.set(userId, socketId);
+        console.log(`➕ User added: ${userId}`);
     }
 
-    addUserToRoom(roomId, userId, socketId, color) {
-        const room = this.getRoom(roomId);
-        room.users.set(userId, {
-            id: userId,
-            socketId,
-            color,
-            joinedAt: Date.now()
-        });
+    removeUser(userId) {
+        this.users.delete(userId);
+        console.log(`➖ User removed: ${userId}`);
     }
 
-    removeUserFromRoom(roomId, userId) {
-        const room = this.getRoom(roomId);
-        if (room) {
-            room.users.delete(userId);
-        }
-    }
-
-    getUsersInRoom(roomId) {
-        const room = this.getRoom(roomId);
-        return Array.from(room.users.values());
-    }
-
-    getRoomForSocket(socketId) {
-        for (const [roomId, room] of this.rooms) {
-            for (const [userId, user] of room.users) {
-                if (user.socketId === socketId) {
-                    return { roomId, user };
-                }
+    getUserBySocketId(socketId) {
+        for (const [userId, sid] of this.users.entries()) {
+            if (sid === socketId) {
+                return userId;
             }
         }
         return null;
     }
 
-    getAllRooms() {
-        return Array.from(this.rooms.keys());
+    getActiveUsers() {
+        return Array.from(this.users.keys());
+    }
+
+    getUserCount() {
+        return this.users.size;
     }
 }
 
